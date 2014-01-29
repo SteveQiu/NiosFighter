a6d51c79b1a724a322cace6ecdcd33a27dd7e138
Sam Coulter <sam.w.coulter@gmail.com>
2014-01-28 20:16:02 -0800

    Added some comments explaining how to use the frametimer

--- software/NiosFighter/frameTimer.h
+++ software/NiosFighter/frameTimer.h
@@ -7,6 +7,8 @@
 //3. Go to Settings -> Common -> hal -> timestamp timer
 //4. Set the value drowpdown to timer_0
 //5. Click "Generate" on the bottom right!
+//DONT FUCK WITH THE TIMER. alt_timestamp_start() SHOULD BE CALLED ONCE
+//WHEN THE frameTimer OBJECT IS INITIALIZED! DONT FUCK WITH IT!
 
 typedef struct frameTimer {
 	int totalTime;
@@ -18,6 +20,9 @@ typedef struct frameTimer {
 int initFrameTimer(frameTimer *ftimer);
 void startFrame(frameTimer *ftimer);
 void endFrame(frameTimer *ftimer);
+//This is the function whos value is passed to updateGame
+//This function returns the time (in seconds) it took for the last
+//frame to execute
 float frameLength(frameTimer *ftimer);
 
 
