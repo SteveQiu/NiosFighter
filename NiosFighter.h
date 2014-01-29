8c055cda30b3eaca0673b77d4da92d0c1b3d3db5
Sam Coulter <sam.w.coulter@gmail.com>
2014-01-28 16:18:53 -0800

    moved source files to top level directory

---
+++ NiosFighter.h
@@ -0,0 +1,7 @@
+#include <altera_up_avalon_video_pixel_buffer_dma.h>
+#include <altera_up_avalon_video_character_buffer_with_dma.h>
+#include <system.h>
+#include <assert.h>
+
+void DrawBackground();
+void DrawCharTest();
