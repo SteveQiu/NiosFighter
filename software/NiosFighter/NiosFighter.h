6702e209102ca02c9054c16dbd36042c5743af74
Sam Coulter <sam.w.coulter@gmail.com>
2014-01-28 16:39:43 -0800

    Deleting old files

--- software/NiosFighter/NiosFighter.h
+++
@@ -1,7 +0,0 @@
-#include <altera_up_avalon_video_pixel_buffer_dma.h>
-#include <altera_up_avalon_video_character_buffer_with_dma.h>
-#include <system.h>
-#include <assert.h>
-
-void DrawBackground();
-void DrawCharTest();
